f61ffa425b5da9257eb72f834315f7a84ef01824
suyash jain <suyashjai2010@gmail.com>
2026-04-05 21:27:31 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Rocket } from "lucide-react"
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+
+      <Rocket className="mr-2 h-4 w-4" />
+      Launch App
+
+    </div>
+  )
+}
